a2860bbe5ebd4c8462a2851ffe6377585d929d70
Loué <emilien.private@gmail.com>
2023-10-10 11:18:49 +0200

    change tests_bank

--- python/tests/test_bank.py
+++ python/tests/test_bank.py
@@ -10,12 +10,12 @@ from .test_data_builder import BankBuilder
 class TestBank:
     def test_convert_currency_euro_to_usd_returns_right_amount(self):
         #Arrange
-        bankbuilder = BankBuilder().with_currency_init(Currency.EUR).with_currency_final(Currency.USD).with_exchange_rates(1.2).build()
+        bank = BankBuilder().with_currency_init(Currency.EUR).with_currency_final(Currency.USD).with_exchange_rates(1.2).build()
         money_initial = Money(10, Currency.EUR)
         currency_final = Currency.USD
         
         # Act
-        result = bankbuilder.convert_currency(money_initial, currency_final)
+        result = bank.convert_currency(money_initial, currency_final)
         print(f'money currency {money_initial.currency}, currency {currency_final}')
         #Assert
         assert result == 12
@@ -23,12 +23,12 @@ class TestBank:
 
     def test_convert_currency_euro_to_usd_returns_same_value(self):
         # Arrange
-        bankbuilder = BankBuilder().with_currency_init(Currency.EUR).with_currency_final(Currency.EUR).with_exchange_rates(1.2).build()
+        bank = BankBuilder().with_currency_init(Currency.EUR).with_currency_final(Currency.EUR).with_exchange_rates(1.2).build()
         money_initial = Money(10, Currency.EUR)
         currency_final = Currency.EUR
         
         # Act
-        result = bankbuilder.convert_currency(money_initial, currency_final)
+        result = bank.convert_currency(money_initial, currency_final)
         
         # Assert
         assert result == 10
@@ -36,34 +36,34 @@ class TestBank:
     def test_convert_currency_with_missing_exchange_rate_throws_exception(self):
         with pytest.raises(MissingExchangeRateError) as error:
             #Arrange
-            bankbuilder = BankBuilder().with_currency_init(Currency.EUR).with_currency_final(Currency.USD).with_exchange_rates(1.2).build()
+            bank = BankBuilder().with_currency_init(Currency.EUR).with_currency_final(Currency.USD).with_exchange_rates(1.2).build()
             
             money_initial = Money(10, Currency.EUR)
             currency_final = Currency.KRW
             
             #Act
 
-            bankbuilder.convert_currency(money_initial, currency_final)
+            bank.convert_currency(money_initial, currency_final)
             
         #Assert
         assert str(error.value) == "EUR->KRW"
 
     def test_convert_currency_with_different_exchange_rate_returns_different_floats(self):
         # Arrange
-        bankbuilder = BankBuilder().with_currency_init(Currency.EUR).with_currency_final(Currency.USD).with_exchange_rates(1.2).build()
+        bank = BankBuilder().with_currency_init(Currency.EUR).with_currency_final(Currency.USD).with_exchange_rates(1.2).build()
         money_initial = Money(10, Currency.EUR)
         currency_final = Currency.USD
         
         # Act
-        result = bankbuilder.convert_currency(money_initial, currency_final)
+        result = bank.convert_currency(money_initial, currency_final)
         
         # Assert
         assert result == 12
         
         #Act
-        bankbuilder.add_exchange_rate(Currency.EUR, Currency.USD, exchange_rate = 1.3)
+        bank.add_exchange_rate(Currency.EUR, Currency.USD, exchange_rate = 1.3)
         
         #Assert
-        assert bankbuilder.convert_currency(money_initial, Currency.USD) == 13
+        assert bank.convert_currency(money_initial, Currency.USD) == 13
 
 
